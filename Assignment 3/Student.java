2178397f41ed7c1ad91c19dc7b1a3c197489b658
Aadith Sukumar <62766172+aadi1011@users.noreply.github.com>
2023-02-22 11:57:53 +0530

    Update Student.java

--- Assignment 3/Student.java
+++ Assignment 3/Student.java
@@ -1,15 +1,30 @@
+// Assignment 3 - Student Management System
+
+/* Student.java contains the class student and uses getMethod and setMethod to extract and set specific values for the variables
+*  StudentDemo.java works on these functions by having operations like:
+*  Add students, Display, Search (by prn, by name, by position), Update/Edit and Delete. */
+
+//Code by Aadith Sukumar (https://www.github.com/aadi1011)
+// AIML A1 | 21070126003
+
+
 public class Student 
 {
+    //initializing the variables
     private int prn;
     private String name;
     private String DOB;
     private int marks;
+    
+    //creating constructor for each of the variable
     Student(int p, String nm, String db, int m) {
         prn = p;
         name = nm;
         DOB = db;
         marks = m;
     }
+    
+    //functions to get and set PRN
     public int getPrn() {
         return prn;
     }
@@ -17,6 +32,7 @@ public class Student
         prn = p;
     }
 
+    //functions to get and set Name
     public String getName() {
         return name;
     }
@@ -24,6 +40,7 @@ public class Student
         name = n;
     }
 
+    //functions to get and set DOB
     public String getDOB() {
         return DOB;
     }
@@ -31,6 +48,7 @@ public class Student
         DOB = d;
     }
 
+    //functions to get and set Marks
     public int getMarks() {
         return marks;
     }
@@ -38,3 +56,4 @@ public class Student
         marks = m;
     }
 }
+//EOF
