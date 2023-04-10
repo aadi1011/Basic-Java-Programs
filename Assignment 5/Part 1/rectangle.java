2bc10498bd85fcd2bb2c318486b21665fbeba7e6
Aadith Sukumar <62766172+aadi1011@users.noreply.github.com>
2023-04-10 16:25:59 +0530

    Create rectangle.java

---
+++ Assignment 5/Part 1/rectangle.java
@@ -0,0 +1,10 @@
+package ShapesInterface;
+
+public class rectangle implements shape {
+    
+    float rectangleArea, rectanglePerimeter, length, breadth;
+
+    public rectangle (float length, float breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
